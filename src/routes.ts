c8f77484d67bb07f0e8a0b09cc140c7bbe62f4b5
Alex Elias <alex.elias@edu.pucrs.br>
2022-02-25 21:07:34 -0300

    create tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
 
 const router = Router();
 
 router.post("/users", CreateUserController)
+router.post("/tags", CreateTagController)
 
 export { router }
