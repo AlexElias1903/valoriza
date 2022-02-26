7a55fad7682459f55e5ebb2c6642af1a51d5eb1d
Alex Elias <alex.elias@edu.pucrs.br>
2022-02-25 21:18:51 -0300

    middleware autentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 router.post("/users", CreateUserController)
-router.post("/tags", CreateTagController)
+router.post("/tags", ensureAdmin, CreateTagController)
 
 export { router }
